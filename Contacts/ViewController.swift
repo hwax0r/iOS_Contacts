2d68cb0f1490c948ae485aa775aad34cb54b5dd9
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-12-09 03:28:06 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by David Sergeev on 09.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
